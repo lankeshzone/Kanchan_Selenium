9ed3dd77d9691b79ff82b7098ad8b509ea06979d
lankeshleader <49361514+lankeshleader@users.noreply.github.com>
2020-01-14 17:32:51 +0530

    latest classes

---
+++ Kanchan_Selenium/collectionEx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+
+namespace Kanchan_Selenium
+{
+    class collectionEx
+    {
+
+        public static void Main(String[] args)
+        {
+            ArrayList a = new ArrayList();
+
+            HashSet<int> hs1 = new HashSet<int>();
+
+            IDictionary<int, string> id = new SortedDictionary<int, string>();
+            
+
+            
+
+
+            a.Add("Rajesh");
+            a.Add(100);
+            a.Add('c');
+            a.Add(10.25f);
+
+            foreach (object x in a)
+                Console.WriteLine(x);
+
+            Console.WriteLine(a.Contains('c'));
+
+            Console.WriteLine(a.Count);
+
+            Console.WriteLine(a.IndexOf(10.25f));
+            ArrayList b = new ArrayList();
+            b.Add(23);
+            b.Add("Beena");
+
+            a.AddRange(b);
+            object[] s = new object[100];
+            a.CopyTo(s);
+
+            for (int d = 0; d < s.Length; d++)
+                Console.WriteLine(s[d]);
+
+            a.Insert(3, "Kanchan");
+            foreach (object x in a)
+                Console.WriteLine(x);
+
+            Console.WriteLine(a.IsFixedSize);
+
+            a.Remove("Beena");
+            foreach (object x in a)
+                Console.WriteLine(x);
+
+            a.Reverse();
+            foreach (object x in a)
+                Console.WriteLine(x);
+
+            Hashtable hs = new Hashtable();
+            hs.Add(100, "Lankesh");
+            hs.Add(23, "Usha");
+            hs.Add(235, "Rajesh");
+            hs.Add(21, "Beena");
+
+            ICollection c = hs.Keys;
+            ICollection v = hs.Values;
+
+            foreach (object aa in c)
+                Console.WriteLine(aa);
+
+
+            foreach (object aa in v)
+                Console.WriteLine(aa);
+
+            hs.Remove(235);
+            Console.WriteLine(hs.Count);
+            Console.WriteLine(hs.ContainsKey(100));
+
+            SortedList sl = new SortedList();
+            
+
+            Console.ReadKey();
+        }
+    }
+
+    class idictionaryEx
+    {
+        public static void Main(String[] args)
+        {
+            OrderedDictionary od = new OrderedDictionary();
+
+            od.Add(100, "Lankesh");
+            od.Add(200, "Kanchan");
+            od.Add(23, "Beena");
+            od.Add(34, "Jashuth");
+
+            Console.WriteLine(od.Contains(34));
+
+            Console.WriteLine(od.Count);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Selenium_Sessions/BrowserLaunch.cs
@@ -0,0 +1,75 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Sessions
+{
+    class BrowserLaunch
+    {
+        public static void Main(String[] args)
+        {
+
+            // created an instance of the chrome browser..
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.linkedin.com";
+
+            //Basic method on WebDriver interface
+
+            Console.WriteLine("Title of the page " + driver.Title);
+
+            Console.WriteLine("Current URL " + driver.Url);
+
+            // Console.WriteLine("Source code of the page" + driver.PageSource);
+
+          //  driver.Manage().Window.Maximize();
+
+        //    driver.Manage().Window.Minimize();
+
+           // driver.Manage().Window.FullScreen();
+
+            Console.WriteLine("Position of the windoow " +driver.Manage().Window.Position);
+
+            Console.WriteLine("Size of the Window " + driver.Manage().Window.Size);
+
+
+            Console.WriteLine("All cookies " + driver.Manage().Cookies.AllCookies);
+
+            driver.Manage().Cookies.DeleteAllCookies();
+
+            Console.WriteLine(" after deletion " + driver.Manage().Cookies);
+
+            Size d = new Size(10, 10);
+
+          //  driver.Manage().Window.Size = d;
+
+          //  driver.Url = "https://www.google.com";
+
+           // driver.Navigate().Back();
+
+            //driver.Navigate().Forward();
+
+            driver.Navigate().Refresh();
+
+            driver.Navigate().GoToUrl("https://www.facebook.com");
+
+            driver.Quit(); // shut down the browser...
+
+
+            //IWebDriver driver2 = new FirefoxDriver();
+            // driver2.Url = "https://www.facebook.com";
+
+            // IWebDriver driver3 = new EdgeDriver();
+            //  driver3.Url = "https://www.google.com";
+
+
+        }
+         
+    }
+}
