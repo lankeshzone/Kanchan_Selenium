f2ba7e1fecb768668552ac5aa14d2763ff2ce513
lankeshleader <49361514+lankeshleader@users.noreply.github.com>
2020-01-18 12:10:40 +0530

    todays session

--- Selenium_Sessions/BrowserLaunch.cs
+++ Selenium_Sessions/BrowserLaunch.cs
@@ -18,63 +18,75 @@ namespace Selenium_Sessions
 
             // created an instance of the chrome browser..
             IWebDriver driver = new ChromeDriver();
+           
+            //TimeOuts
+           
+            
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
             driver.Url = "https://www.linkedin.com";
+
+
+            driver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(2);
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            //js.ExecuteAsyncScript("alert('Hello');");
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+            driver.Navigate().Refresh();
 
             //Basic method on WebDriver interface
 
-            Console.WriteLine("Title of the page " + driver.Title);
+            /* Console.WriteLine("Title of the page " + driver.Title);
 
-            Console.WriteLine("Current URL " + driver.Url);
+             Console.WriteLine("Current URL " + driver.Url);
 
-            // Console.WriteLine("Source code of the page" + driver.PageSource);
+             // Console.WriteLine("Source code of the page" + driver.PageSource);
 
-            //  driver.Manage().Window.Maximize();
+             //  driver.Manage().Window.Maximize();
 
-            //    driver.Manage().Window.Minimize();
+             //    driver.Manage().Window.Minimize();
 
-            // driver.Manage().Window.FullScreen();
+             // driver.Manage().Window.FullScreen();
 
-            Point p = new Point(100,100);
+             Point p = new Point(100,100);
 
-            driver.Manage().Window.Position=p;
+             driver.Manage().Window.Position=p;
 
+             Console.WriteLine("Position of the windoow " +driver.Manage().Window.Position);
 
-            Console.WriteLine("Position of the windoow " +driver.Manage().Window.Position);
-
-            Console.WriteLine("Size of the Window " + driver.Manage().Window.Size);
-
-
-            Console.WriteLine("All cookies " + driver.Manage().Cookies.AllCookies);
-
-            driver.Manage().Cookies.DeleteAllCookies();
-
-            Console.WriteLine(" after deletion " + driver.Manage().Cookies);
-
-            Size d = new Size(10, 10);
-
-          //  driver.Manage().Window.Size = d;
-
-          //  driver.Url = "https://www.google.com";
-
-           // driver.Navigate().Back();
-
-            //driver.Navigate().Forward();
-
-            driver.Navigate().Refresh();
-
-            driver.Navigate().GoToUrl("https://www.facebook.com");
-
-            driver.Quit(); // shut down the browser...
+             Console.WriteLine("Size of the Window " + driver.Manage().Window.Size);
 
 
-            //IWebDriver driver2 = new FirefoxDriver();
-            // driver2.Url = "https://www.facebook.com";
+             Console.WriteLine("All cookies " + driver.Manage().Cookies.AllCookies);
 
-            // IWebDriver driver3 = new EdgeDriver();
-            //  driver3.Url = "https://www.google.com";
+             driver.Manage().Cookies.DeleteAllCookies();
+
+             Console.WriteLine(" after deletion " + driver.Manage().Cookies);
+
+             Size d = new Size(10, 10);
+
+             //driver.Manage().Window.Size = d;
+
+             //driver.Url = "https://www.google.com";
+
+             //driver.Navigate().Back();
+
+             //driver.Navigate().Forward();
+
+             
+
+             driver.Navigate().GoToUrl("https://www.facebook.com");
+
+             driver.Quit(); // shut down the browser...
+
+
+             //IWebDriver driver2 = new FirefoxDriver();
+             // driver2.Url = "https://www.facebook.com";
+
+             // IWebDriver driver3 = new EdgeDriver();
+             //  driver3.Url = "https://www.google.com";*/
 
 
         }
-         
+
     }
 }
---
+++ Selenium_Sessions/WebElementEx.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Selenium_Sessions
+{
+    class WebElementEx
+    {
+        public static void Main(String[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Url = "https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin";
+
+            IWebElement userID;
+            userID = driver.FindElement(By.Id("username"));
+
+            Console.WriteLine("Height " + userID.GetCssValue("height"));
+            Console.WriteLine("Property " + userID.GetProperty("name"));
+            Console.WriteLine("Attribute " + userID.GetAttribute("tag"));
+            Console.WriteLine("Sixe " + userID.Size);
+            Console.WriteLine("Position " + userID.Location);
+
+            Console.WriteLine("Enabled??? " + userID.Enabled);
+            Console.WriteLine("Displayed?? " + userID.Displayed);
+            Console.WriteLine("Selected ?? " + userID.Selected);
+
+            userID.Click();
+            Thread.Sleep(2);
+            userID.SendKeys("lankeshzone@gmail.com");
+            Thread.Sleep(2);
+            userID.Clear();
+
+            driver.FindElement(By.ClassName("btn__primary--large")).Submit();
+
+        }
+    }
+}
