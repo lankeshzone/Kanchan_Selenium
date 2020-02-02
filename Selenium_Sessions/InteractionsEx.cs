42025b2b894ea891614169a60e88f2e567bf7f94
lankeshleader <49361514+lankeshleader@users.noreply.github.com>
2020-02-02 12:49:52 +0530

    interaction Example

---
+++ Selenium_Sessions/InteractionsEx.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Sessions
+{
+    class InteractionsEx
+    {
+
+        public static void Main(String[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Url = "http://demo.automationtesting.in/Register.html";
+
+            IWebElement interaction, draganddrop, staticEl ;
+
+            interaction = driver.FindElement(By.XPath("//a[contains(text(),'Interactions')]"));
+            draganddrop = driver.FindElement(By.XPath("//a[contains(text(),'Drag and Drop')]"));
+            staticEl = driver.FindElement(By.XPath("//a[contains(text(),'Static')]"));
+
+            interaction.Click();
+            draganddrop.Click();
+            staticEl.Click();
+        }
+    }
+}
